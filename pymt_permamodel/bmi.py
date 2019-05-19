f3691e0d980f5d145d4725dd96eb9c4b80e0fd24
mcflugen <mcflugen@gmail.com>
2019-05-19 16:20:50 -0600

    fix: move metadata into package folder

--- pymt_permamodel/bmi.py
+++ pymt_permamodel/bmi.py
@@ -1,7 +1,19 @@
 from __future__ import absolute_import
+import os
+import pkg_resources
 
 from permamodel.components.bmi_frost_number import BmiFrostnumberMethod as FrostNumber
 from permamodel.components.bmi_Ku_component import BmiKuMethod as Ku
 
-Ku.__name__ = "Ku"
+
 FrostNumber.__name__ = "FrostNumber"
+FrostNumber.METADATA = os.path.abspath(
+    pkg_resources.resource_filename("pymt_permamodel", "data/FrostNumber")
+)
+
+Ku.__name__ = "Ku"
+Ku.METADATA = os.path.abspath(
+    pkg_resources.resource_filename("pymt_permamodel", "data/Ku")
+)
+
+__all__ = ["FrostNumber", "Ku"]
--- setup.py
+++ setup.py
@@ -5,16 +5,22 @@ import sys
 import versioneer
 from setuptools import find_packages, setup
 
-from distutils.extension import Extension
+# from distutils.extension import Extension
 
-from model_metadata.utils import get_cmdclass, get_entry_points
+# from model_metadata.utils import get_cmdclass, get_entry_points
 
 
 packages = find_packages()
-pymt_components = [
-    ("Ku=pymt_permamodel.bmi:Ku", "meta/Ku"),
-    ("FrostNumber=pymt_permamodel.bmi:FrostNumber", "meta/FrostNumber"),
-]
+# pymt_components = [
+#     ("Ku=pymt_permamodel.bmi:Ku", "meta/Ku"),
+#     ("FrostNumber=pymt_permamodel.bmi:FrostNumber", "meta/FrostNumber"),
+# ]
+entry_points = {
+    "pymt.plugins": [
+        "Ku=pymt_permamodel.bmi:Ku",
+        "FrostNumber=pymt_permamodel.bmi:FrostNumber",
+    ]
+}
 
 setup(
     name="pymt_permamodel",
@@ -22,6 +28,7 @@ setup(
     description="PyMT plugin permamodel",
     version=versioneer.get_version(),
     packages=packages,
-    cmdclass=get_cmdclass(pymt_components, cmdclass=versioneer.get_cmdclass()),
-    entry_points=get_entry_points(pymt_components),
+    # cmdclass=get_cmdclass(pymt_components, cmdclass=versioneer.get_cmdclass()),
+    cmdclass=versioneer.get_cmdclass(),
+    entry_points=entry_points,
 )
