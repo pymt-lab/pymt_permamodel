0b324afac84c7892a83f97253321c0de0e76ab3c
mcflugen <mcflugen@gmail.com>
2019-05-21 10:13:40 -0600

    fix: include package data

--- setup.py
+++ setup.py
@@ -5,16 +5,8 @@ import sys
 import versioneer
 from setuptools import find_packages, setup
 
-# from distutils.extension import Extension
-
-# from model_metadata.utils import get_cmdclass, get_entry_points
-
 
 packages = find_packages()
-# pymt_components = [
-#     ("Ku=pymt_permamodel.bmi:Ku", "meta/Ku"),
-#     ("FrostNumber=pymt_permamodel.bmi:FrostNumber", "meta/FrostNumber"),
-# ]
 entry_points = {
     "pymt.plugins": [
         "Ku=pymt_permamodel.bmi:Ku",
@@ -28,7 +20,7 @@ setup(
     description="PyMT plugin permamodel",
     version=versioneer.get_version(),
     packages=packages,
-    # cmdclass=get_cmdclass(pymt_components, cmdclass=versioneer.get_cmdclass()),
     cmdclass=versioneer.get_cmdclass(),
     entry_points=entry_points,
+    include_package_data=True,
 )
