5d7bfc97a491a6684289d3f8ea84e6433a68dea8
Mark Piper <mark.piper@colorado.edu>
2024-11-05 12:33:05 -0700

    Set metadata path with importlib_resources

--- pymt_permamodel/bmi.py
+++ pymt_permamodel/bmi.py
@@ -1,16 +1,32 @@
 from __future__ import absolute_import
 
-import pkg_resources
+import sys
+import importlib
+import pathlib
+
 from permamodel.components.bmi_frost_number import BmiFrostnumberMethod as FrostNumber
 from permamodel.components.bmi_Ku import BmiKuModel as KuEnhanced
 from permamodel.components.bmi_Ku_component import BmiKuMethod as Ku
 
+if sys.version_info >= (3, 12):  # pragma: no cover (PY12+)
+    import importlib.resources as importlib_resources
+else:  # pragma: no cover (<PY312)
+    import importlib_resources
+
 FrostNumber.__name__ = "FrostNumber"
-FrostNumber.METADATA = pkg_resources.resource_filename(__name__, "data/FrostNumber")
+FrostNumber.METADATA = pathlib.Path(
+    importlib_resources.files(__name__) / "data" / FrostNumber.__name__
+)
+
 Ku.__name__ = "Ku"
-Ku.METADATA = pkg_resources.resource_filename(__name__, "data/Ku")
+Ku.METADATA = pathlib.Path(
+    importlib_resources.files(__name__) / "data" / Ku.__name__
+)
+
 KuEnhanced.__name__ = "KuEnhanced"
-KuEnhanced.METADATA = pkg_resources.resource_filename(__name__, "data/KuEnhanced")
+KuEnhanced.METADATA = pathlib.Path(
+    importlib_resources.files(__name__) / "data" / KuEnhanced.__name__
+)
 
 __all__ = [
     "FrostNumber",
